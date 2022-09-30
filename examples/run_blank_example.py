c26a8d438b87e6e46c92a830cd7fb82bd0704d2e
Maya Frai <mayafrai@Mayas-MBP.attlocal.net>
2022-09-29 17:35:52 -0700

    push

--- examples/run_blank_example.py
+++ examples/run_blank_example.py
@@ -10,14 +10,14 @@ from api import demo_web_app
 # Construct GPT object and show some examples
 gpt = GPT(engine="davinci", temperature=0.5, max_tokens=100)
 
-gpt.add_example(Example("Who are you?", "I'm an example."))
-gpt.add_example(Example("What are you?", "I'm an example."))
+gpt.add_example(Example("Explain to me what an LA cool girl would listen to, wear as an outfit, and what books she'd read.", "An LA cool girl would listen to Olivia Rodrigo, wear Alo Yoga clothing, and read Joan Didion."))
+# gpt.add_example(Example("What are you?", "I'm an example."))
 
 # Define UI configuration
 config = UIConfig(
-    description="Prompt",
-    button_text="Result",
-    placeholder="Where are you?",
+    description="Query",
+    button_text="Generate Recommendation",
+    placeholder="NYC cool girl",
     show_example_form=True,
 )
 
